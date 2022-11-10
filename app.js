54923a2ad126edc375911c164bca392bc0f76208
Ritalexi <okoliogochukwu83@gmail.com>
2022-11-10 13:15:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+const loader = document.querySelector("#preloader");
+document.querySelector("#main").style.display = "none";
+loader.classList.add("preloader");
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        loader.classList.remove("preloader");
+        document.querySelector("#main").style.display = "block";
+    }, 2000);
+});
+
+const nav = document.querySelector(".nav");
+const toogleBtn = document.querySelector(".toogle_btn");
+const navList = document.querySelector(".nav_list"); 
+const blueButton = document.querySelector(".blue_button"); 
+
+document.addEventListener('scroll', () => {
+    nav.style.backgroundColor = "#CCE6F1";
+    if (window.scrollY == 0){
+        nav.style.backgroundColor = "transparent";
+    }
+})
+toogleBtn.addEventListener('click', () => {
+    nav.classList.toggle("active");
+    toogleBtn.classList.toggle("is_active");
+    navList.classList.toggle("active");
+    nav.style.backgroundColor = "#cce6f1";
+})
+
+var count = 0
+blueButton.addEventListener('click', () => {
+    if (count == 0){
+        blueButton.style.backgroundColor = "#2B97C6";
+        count = 1
+    }
+    else{
+        blueButton.style.backgroundColor = "#006C9B";
+        count = 0
+    }
+})
+
+const navLists = document.querySelectorAll(".nav_list li a"); 
+    navLists[0].addEventListener('click', () =>{
+            navLists[0].classList.add("first-child")
+            navLists[1].classList.remove("first-child")
+            navLists[2].classList.remove("first-child")
+            navLists[3].classList.remove("first-child")
+            navLists[4].classList.remove("first-child")
+    })
+    navLists[1].addEventListener('click', () =>{
+        navLists[1].classList.add("first-child")
+        navLists[0].classList.remove("first-child")
+        navLists[2].classList.remove("first-child")
+        navLists[3].classList.remove("first-child")
+        navLists[4].classList.remove("first-child")
+})
+navLists[2].addEventListener('click', () =>{
+    navLists[2].classList.add("first-child")
+    navLists[1].classList.remove("first-child")
+    navLists[0].classList.remove("first-child")
+    navLists[3].classList.remove("first-child")
+    navLists[4].classList.remove("first-child")
+})
+navLists[3].addEventListener('click', () =>{
+    navLists[3].classList.add("first-child")
+    navLists[1].classList.remove("first-child")
+    navLists[2].classList.remove("first-child")
+    navLists[0].classList.remove("first-child")
+    navLists[4].classList.remove("first-child")
+})
+navLists[4].addEventListener('click', () =>{
+    navLists[4].classList.add("first-child")
+    navLists[1].classList.remove("first-child")
+    navLists[2].classList.remove("first-child")
+    navLists[3].classList.remove("first-child")
+    navLists[0].classList.remove("first-child")
+})
+
+const emailMessage = document.querySelector('.error-message');
+const email = document.querySelector('#email');
+const emailSubmit = document.querySelector('.email_submit')
+emailSubmit.addEventListener('click', () => {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.value == "" || email.value == "null"){
+emailMessage.innerHTML = "Please enter valid email address.";
+emailMessage.style.display = "block";
+email.style.borderColor = "#B01A1A";
+    }
+    if(!email.value.match(mailformat)){
+        emailMessage.innerHTML = "Please enter valid email address.";
+        emailMessage.style.display = "block";
+        email.style.borderColor = "#B01A1A";
+    }else{
+        emailMessage.innerHTML = "";
+        emailMessage.style.display = "none";
+        email.style.borderColor = "#8A8B8B";
+    }
+})
